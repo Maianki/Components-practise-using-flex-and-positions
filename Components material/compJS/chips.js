73cfe1e3121932466ebd07a4af5bc930ed1f7429
Ankit Kumain <kumankit10@gmail.com>
2022-01-21 13:00:27 +0530

    style: auto wrap chips

--- Components material/compJS/chips.js
+++ Components material/compJS/chips.js
@@ -1,12 +1,21 @@
 const txtInput = document.querySelector(".input-box")
+const closeBtn = document.querySelector(".close-btn")
+const inputChipsDisplay = document.querySelector(".input-chips-display")
 
-const addChip = ()=>{
-
-}
 txtInput.addEventListener("keyup",(event)=>{
     if (event.key === "Enter") {
-        addChip(event.target.value);
         console.log(event.target.value)
+        const chipInnertext = event.target.value
+        const newChip = document.createElement("div");
+        newChip.innerHTML = `${chipInnertext} <i class="close-btn fa fa-close"></i>`;
+        newChip.className = "chip"
+        inputChipsDisplay.appendChild(newChip);
         event.target.value = "";
       }
+     
 })
+
+// closeBtn.addEventListener('click',(e)=>{
+//   console.log(closeBtn)
+//   closeBtn.parentElement.style.display='none'
+// })
