a23a9f8f657b40979abefb7ccab88a73a2efe2d8
Ankit Kumain <kumankit10@gmail.com>
2022-01-23 11:19:41 +0530

    add stacked snackbar

--- Components material/compJS/snackbar.js
+++ Components material/compJS/snackbar.js
@@ -1,10 +1,13 @@
 const buttonDemoLeading = document.querySelector(".btn-demo-leading");
 const buttonDemoBaseline = document.querySelector(".btn-demo-baseline");
+const buttonDemoStacked = document.querySelector(".btn-demo-stacked");
 const demoToast = document.querySelector(".demo-toast")
 const btnRetry = document.querySelector(".demo-retry")
+const stackedToast= document.querySelector(".stacked-toast")
+
 
 buttonDemoLeading.addEventListener("click",()=>{
-    demoToast.classList.remove("baseline-snackbar");
+    demoToast.classList.remove("leading-snackbar");
     demoToast.style.display = "flex"
     setTimeout(()=>{
         demoToast.style.display = "none"
@@ -12,9 +15,17 @@ buttonDemoLeading.addEventListener("click",()=>{
 })
 
 buttonDemoBaseline.addEventListener("click",()=>{
-    demoToast.classList.add("baseline-snackbar");
+    demoToast.classList.add("center-snackbar");
     demoToast.style.display = "flex"
     setTimeout(()=>{
         demoToast.style.display = "none"
+    },2000)
+})
+
+buttonDemoStacked.addEventListener("click",()=>{
+    stackedToast.classList.add("center-snackbar");
+    stackedToast.style.display = "flex"
+    setTimeout(()=>{
+        stackedToast.style.display = "none"
     },2000)
 })
